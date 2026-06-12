ae326df9f7a417fcfdc9487c79457ad1cd9ea30f
Luis <luisromoportillo@gmail.com>
2026-06-11 21:01:47 -0700

    fix: update Tailwind CSS integration and configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
-// astro.config.mjs
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  integrations: [tailwind()],
-});
+export default defineConfig({});
